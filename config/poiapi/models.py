594e29fc30dea29c4eecec4857fab38d139de852
fahrury-rdd27 <fahrury_rdd27@apps.ipb.ac.id>
2021-06-14 14:26:57 +0700

    change def point location in model

--- config/poiapi/models.py
+++ config/poiapi/models.py
@@ -6,7 +6,7 @@ from django.contrib.gis.geos import Point
 
 class PointOfInterest(models.Model):
     place_name = models.CharField(max_length=30, null=False, blank=False)
-    location = models.PointField(geography=True, default=Point(-6.555760409044816, 106.7264194085899))
+    location = models.PointField(geography=True, default=Point(106.7264194085899, -6.555760409044816))
     current_visitors = models.IntegerField(default=0, blank=False)
     peak_hours = models.BooleanField(default=False, blank=False)
 
